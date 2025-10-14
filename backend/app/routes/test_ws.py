e8d76dff69426196287eec08c51f431743cd68b1
Justin Paul Palomiano Morada <justinpaulmorada969@gmail.com>
2025-10-14 16:59:54 +0800

    Debugging WebSocket

--- backend/app/routes/test_ws.py
+++
@@ -1,9 +0,0 @@
-# backend/app/routes/test_ws.py
-from fastapi import APIRouter, WebSocket
-
-router = APIRouter()
-
-@router.websocket("/ws/test/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: int):
-    await websocket.accept()
-    await websocket.send_text(f"Hello {user_id}")

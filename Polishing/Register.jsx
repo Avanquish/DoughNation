872bc793aeac643070a26b2308c0977f5429c310
mesyjejeii <smaestro.college@gmail.com>
2025-11-24 21:34:05 +0800

    Add files via upload

--- Polishing/Register.jsx
+++ Polishing/Register.jsx
@@ -626,7 +626,7 @@ export default function Register() {
                         handleInputChange("confirm_password", e.target.value)
                       }
                       required
-                      placeholder="Re-enter new password"
+                      placeholder="Re-enter password"
                       className="appearance-none pr-11 bg-white/85 border-[#FFE1BE] text-[#6c471d] placeholder:text-[#E3B57E] focus-visible:ring-[#E3B57E]"
                       style={{ height: "clamp(44px, 5.5svh, 52px)" }}
                     />
